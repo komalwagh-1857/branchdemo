dc7829d8e1b114f9500eedc8b99a88e637b351af
komalwagh-1857 <komalwagh6399@gmail.com>
2022-09-27 09:39:32 +0530

    my first commit

---
+++ src/main/java/Branch/pullrc.java
@@ -0,0 +1,13 @@
+package Branch;
+
+public class pullrc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+	
+int a=10;
+int b=20;
+
+System.out.println(a+b);
+}
+}
